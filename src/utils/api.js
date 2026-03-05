43442a702db6fa9988a678b1b1db92b8e9c7ba8b
JhonierMejia10 <124406476+JhonierMejia10@users.noreply.github.com>
2026-03-04 22:12:30 -0500

    Se añadió la url en producción

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const isDevelopment = import.meta.env.MODE === 'development'
+const myBaseUrl = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_DEPLOY
+
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: myBaseUrl,
     headers: {
         'Content-Type': 'application/json',
     },
